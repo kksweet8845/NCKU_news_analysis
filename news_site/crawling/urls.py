dcac1be20a39bee988078588b30e32d7ad68b242
f74064054@mail.ncku.edu.tw <f74064054@mail.ncku.edu.tw>
2020-03-04 21:41:23 +0800

    Modify to support new test

--- news_site/crawling/urls.py
+++ news_site/crawling/urls.py
@@ -1,9 +1,13 @@
 from rest_framework import routers
 from django.urls import path
-from .views import test_ltn_crawling
+from .views import test_ltn_crawling, test_nowNews_crawling, test_pts_crawling, test_udn_crawling, test_cts_crawling
 
 
 
 urlpatterns = [
-    path('api/ltn/', test_ltn_crawling, name='test ltn')
+    path('api/ltn/', test_ltn_crawling, name='test ltn'),
+    path('api/nowNews/', test_nowNews_crawling, name='test nowNews'),
+    path('api/pts/', test_pts_crawling, name='test pts'),
+    path('api/udn/', test_udn_crawling, name='test udn'),
+    path('api/cts/', test_cts_crawling, name='test cts')
 ]
--- news_site/crawling/views.py
+++ news_site/crawling/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .api import ltn_crawling
+from crawling.apis import ltn_crawling, nowNews_crawling, pts_crawling, udn_crawling, cts_crawling
 from newsdb.models import Subject, Brand, Brand_sub
 # Create your views here.
 
 def test_ltn_crawling(request):
     c = ltn_crawling()
+    # data = c.getNews(date='all')
+    data = c.getNewsToday()
+    # result = c.insertNews(data)
+    return HttpResponse(data)
+
+def test_nowNews_crawling(request):
+    c = nowNews_crawling()
     data = c.getNews(date='all')
     result = c.insertNews(data)
+    return HttpResponse(data[:500])
+
+def test_pts_crawling(request):
+    c = pts_crawling()
+    data = c.getNews(date='all')
+    return HttpResponse(data)
+
+def test_udn_crawling(request):
+    c = udn_crawling()
+    data = c.getNews(date='all')
+    return HttpResponse(data)
+
+def test_cts_crawling(request):
+    c = cts_crawling()
+    data = c.getNews(date='all')
     return HttpResponse(data)
