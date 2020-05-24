33e752b3ea7cc57f34ac9c6c0db9aae1ea3fdd4c
F74066187 <dio23310045123@gmail.com>
2020-05-24 16:15:52 +0800

    implent pie chart and bar chart

--- news_site/frontend/src/components/common/pieChart.js
+++ news_site/frontend/src/components/common/pieChart.js
@@ -5,26 +5,32 @@ import Chartist from 'chartist'
 import { makeStyles } from '@material-ui/core/styles';
 import { CircularProgress } from '@material-ui/core'
 import color from '@material-ui/core/colors/amber';
+import legend from 'chartist-plugin-legend'
 
+import './md5'
 import '../css/pieChart.css'
 
 const renderPie = (grades, nodeId)=>{
     let chart = new Chartist.Pie(`.${nodeId}`, {
-        series: [
-            {
-                value: grades[0],
-                className: 'positive-stroke',
-            },
-            {
-                value: grades[1],
-                className: 'neutral-stroke',
-            },
-            {
-                value: grades[2],
-                className: 'negative-stroke',
-            }
-        ],
-        labels: [1, 2, 3],
+            series: [
+                {
+                    value: grades[0],
+                    className: 'positive-stroke',
+                },
+                {
+                    value: grades[1],
+                    className: 'neutral-stroke',
+                },
+                {
+                    value: grades[2],
+                    className: 'negative-stroke',
+                }
+            ],
+            showLabel: false,
+            labels: [1, 2, 3],
+            plugins: [
+                legend(),
+            ]
         }, {
             donut: true,
             showLabel: false
@@ -64,17 +70,21 @@ const renderPie = (grades, nodeId)=>{
         }
     });
     // For the sake of the example we update the chart every time it's created with a delay of 8 seconds
-    chart.on('created', function() {
-        if(window.__anim21278907124) {
-        clearTimeout(window.__anim21278907124);
-        window.__anim21278907124 = null;
-    }
-        window.__anim21278907124 = setTimeout(chart.update.bind(chart), 5000);
-    });
+    // chart.on('created', function() {
+    //     if(window.__anim21278907124) {
+    //     clearTimeout(window.__anim21278907124);
+    //     window.__anim21278907124 = null;
+    // }
+    //     window.__anim21278907124 = setTimeout(chart.update.bind(chart), 5000);
+    // });
 }
 
 export default function (props) {
+
+    props.nodeId = '_' + parseInt(Math.random()*100000).toString()
+
     useEffect(()=>{
+        console.log(props.nodeId)
         renderPie(props.grades, props.nodeId)
     })
 
